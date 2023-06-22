a053398fb2b2f8daad2057e73365f71338658d95
Akagami no Shanks <89997083+MustafaAlibekov@users.noreply.github.com>
2023-06-22 15:24:06 +0300

    aaw

---
+++ ClassLibrary2/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ClassLibrary2
+{
+    public class Password
+    {
+        public static string checkPassword(string password)
+        {
+            string correct = "хорошо";
+            string error = "ещё";
+            string symbol = "!@#$%^&*()";
+            if (password.Length >= 6)
+            {
+                string erroq = "Нужны спец символ";
+                if (password.Intersect(symbol).Count() != 0)
+                {
+                    string errow = "нужны заглавные";
+                    if (password.Any(Char.IsUpper))
+                    {
+                        string errort = "нужны строчные";
+                        if (password.Any(Char.IsLower))
+                        {
+                            string errore = "Нужны цифры";
+                            if (password.Any(Char.IsDigit))
+                            {
+                                return correct;
+                            }
+                            else return correct;
+                        }
+                        else return correct;
+                    }
+                    else return correct;
+                }
+                else return correct;
+            }
+            else return correct;
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using ClassLibrary2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class PasswordTest
+    {
+        [TestMethod]
+        public void CheckPassword()
+        {
+            string password = "qwerty1234Q#";
+            string expected = "хорошо";
+
+            string actual = Password.checkPassword(password);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CheckPassword1()
+        {
+            string password = "Dis23$23";
+            string expected = "хорошо";
+
+            string actual = Password.checkPassword(password);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CheckPassword2()
+        {
+            string password = "Alex200#1";
+            string expected = "хорошо";
+
+            string actual = Password.checkPassword(password);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
